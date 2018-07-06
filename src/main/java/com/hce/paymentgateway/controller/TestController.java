985d1bb596b851febafbbbff4e564f53381c5e1f
Qiang Ma <mq7253180@126.com>
2018-07-06 15:15:02 +0800

    test

---
+++ src/main/java/com/hce/paymentgateway/controller/TestController.java
@@ -0,0 +1,30 @@
+package com.hce.paymentgateway.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+	@RequestMapping(value = "/commandline")
+	public String testCommandLine() throws IOException, InterruptedException {
+		String cmd = "ps -ef|grep java|grep -v grep";
+        Process process = Runtime.getRuntime().exec(cmd);
+        process.waitFor();//阻塞，等待脚本执行完
+        InputStream in = null;
+        try {
+        	in = process.getInputStream();
+        	byte[] buf = new byte[in.available()];
+        	in.read(buf);
+        	String result = new String(buf);
+        	System.out.println("---------------"+result);
+        	return result;
+        } finally {
+        	if(in!=null)
+        		in.close();
+        }
+	}
+}
