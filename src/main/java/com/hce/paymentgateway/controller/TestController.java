59329a12ec9c99fd68db39501cd351bf8191191b
Qiang Ma <mq7253180@126.com>
2018-07-06 15:47:03 +0800

    test

--- src/main/java/com/hce/paymentgateway/controller/TestController.java
+++ src/main/java/com/hce/paymentgateway/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.hce.paymentgateway.controller;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -19,20 +20,27 @@ public class TestController {
 	@RequestMapping(value = "/commandline")
 	@ResponseBody
 	public String testCommandLine() throws IOException, InterruptedException {
-		String cmd = "sudo gpg -o /home/wsh/decryption2.txt -d /tmp/UFF1.STP.HKHCEH.HKHCEH.201807060012.txt.DHBKHKHH.D20180706T153341.ACK1.pgp";
+		String path = "/home/wsh/decryption2.txt";
+		String cmd = "sudo gpg -o "+path+" -d /tmp/UFF1.STP.HKHCEH.HKHCEH.201807060012.txt.DHBKHKHH.D20180706T153341.ACK1.pgp";
         Process process = Runtime.getRuntime().exec(cmd);
         process.waitFor();//阻塞，等待脚本执行完
         InputStream in = null;
+        InputStream fileIn = null;
         try {
         	in = process.getInputStream();
+        	fileIn = new FileInputStream(path);
         	byte[] buf = new byte[in.available()];
         	in.read(buf);
         	String result = new String(buf);
         	System.out.println(in.available()+"---------------"+result);
-        	return result;
+        	buf = new byte[fileIn.available()];
+        	fileIn.read(buf);
+        	return result+"\r\n"+new String(buf);
         } finally {
         	if(in!=null)
         		in.close();
+        	if(fileIn!=null)
+        		fileIn.close();
         }
 	}
 }
