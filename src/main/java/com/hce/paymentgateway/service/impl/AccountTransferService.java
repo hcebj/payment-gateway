f9391689f45877af1c4d884d6f61127846223609
wangshaohua <wangshaohuayh@163.com>
2018-07-11 21:02:58 +0800

    xxx

--- src/main/java/com/hce/paymentgateway/service/impl/AccountTransferService.java
+++ src/main/java/com/hce/paymentgateway/service/impl/AccountTransferService.java
@@ -5,6 +5,7 @@ import com.hce.paymentgateway.api.hce.AccountTransferRequest;
 import com.hce.paymentgateway.dao.AccountTransferDao;
 import com.hce.paymentgateway.entity.AccountTransferEntity;
 import com.hce.paymentgateway.util.Constant;
+import com.hce.paymentgateway.util.FileNameGenerator;
 import com.hce.paymentgateway.util.ServiceParameter;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.SftpException;
@@ -41,6 +42,7 @@ public class AccountTransferService extends AbstractTransactionService<AccountTr
         BeanUtils.copyProperties(tradeRequest, transfer);
         transfer.setStatus(PROCESSING.getStatus());
         transfer.setTransactionStatus("SEND");
+        transfer.setFileName(FileNameGenerator.generateRequestFileName(tradeRequest));
         accountTransferDao.save(transfer);
         log.info("[网关支付]数据入库成功, id = {}, transId= {}", transfer.getId(), transfer.getTransId());
         ftpRequestDBS(tradeRequest, transfer);
--- src/main/java/com/hce/paymentgateway/service/impl/ChatsPaymentService.java
+++ src/main/java/com/hce/paymentgateway/service/impl/ChatsPaymentService.java
@@ -17,6 +17,7 @@ import com.hce.paymentgateway.api.hce.TradeResponse;
 import com.hce.paymentgateway.dao.AccountTransferDao;
 import com.hce.paymentgateway.entity.AccountTransferEntity;
 import com.hce.paymentgateway.util.Constant;
+import com.hce.paymentgateway.util.FileNameGenerator;
 import com.hce.paymentgateway.util.ServiceParameter;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.SftpException;
@@ -42,6 +43,7 @@ public class ChatsPaymentService extends AbstractTransactionService<ChatsPayment
         BeanUtils.copyProperties(tradeRequest, transfer);
         transfer.setStatus(PROCESSING.getStatus());
         transfer.setTransactionStatus("SEND");
+        transfer.setFileName(FileNameGenerator.generateRequestFileName(tradeRequest));
         accountTransferDao.save(transfer);
         log.info("[网关支付]数据入库成功, id = {}, transId= {}", transfer.getId(), transfer.getTransId());
         ftpRequestDBS(tradeRequest, transfer);
