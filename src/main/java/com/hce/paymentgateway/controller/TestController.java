20d549d77cada8488ef9718decdd5992e634dc2d
Qiang Ma <mq7253180@126.com>
2018-07-11 20:55:58 +0800

    xxx

--- src/main/java/com/hce/paymentgateway/controller/TestController.java
+++ src/main/java/com/hce/paymentgateway/controller/TestController.java
@@ -12,21 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hce.paymentgateway.service.ResponseProcessService;
-import com.hce.paymentgateway.service.SecretService;
-
 @RestController
 @RequestMapping(value = "/test")
 public class TestController {
-	@Autowired
-	private SecretService secretService;
-	@Autowired
-	private ResponseProcessService accountingService;
+//	@Autowired
+//	private SecretService secretService;
+//	@Autowired
+//	private ResponseProcessService accountingService;
 
 	@RequestMapping(value = "/commandline")
 	@ResponseBody
 	public String testCommandLine() throws IOException, InterruptedException {
-		return secretService.pgp("/tmp/UFF1.STP.HKHCEH.HKHCEH.201807060012.txt.DHBKHKHH.D20180706T153341.ACK1.pgp", "/home/wsh/decryption2.txt");
+//		return secretService.pgp("/tmp/UFF1.STP.HKHCEH.HKHCEH.201807060012.txt.DHBKHKHH.D20180706T153341.ACK1.pgp", "/home/wsh/decryption2.txt");
+		return null;
 	}
 
 	@RequestMapping(value = "/test")
@@ -36,7 +34,7 @@ public class TestController {
 		files.add(new File("D:/docs/vareport/HKHCEHXXXXXX.HK_0248_HKD_EPAYCOL.ENH.001.D180709T203218.csv"));
 		files.add(new File("D:/docs/vareport/HKHCEHXXXXXX.VARPT.HK.0248.HKD.TRAN.ENH.D180709T203218.csv"));
 		files.add(new File("D:/docs/vareport/HKHCEHXXXXXX_DSG_VAHKL_RESP_09072018203232.xls"));
-		accountingService.process(files);
+//		accountingService.process(files);
 		return "xxx";
 //		return "DECRYPTION: "+secretService.test("/tmp/UFF1.STP.HKHCEH.HKHCEH.201807060012.txt.DHBKHKHH.D20180706T153341.ACK1.pgp", "/tmp/UFF1.STP.HKHCEH.HKHCEH.201807060012.txt.DHBKHKHH.D20180706T153341.ACK1");
 //		return "DECRYPTION: "+secretService.test("D:/dbs/UFF1.STP.HKHCEH.HKHCEH.201807060012.txt.DHBKHKHH.D20180706T153341.ACK1.pgp", "D:/dbs/decryption.txt");
