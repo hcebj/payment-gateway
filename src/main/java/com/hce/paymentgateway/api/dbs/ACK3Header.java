7ef12bc1837151142a971edd15e02c2ff982cb8e
wangshaohua <wangshaohuayh@163.com>
2018-07-11 18:21:00 +0800

    xxxx

--- src/main/java/com/hce/paymentgateway/api/dbs/ACK3Header.java
+++ src/main/java/com/hce/paymentgateway/api/dbs/ACK3Header.java
@@ -1,8 +1,34 @@
 package com.hce.paymentgateway.api.dbs;
 
+import com.hce.paymentgateway.util.Order;
+
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @Author Heling.Yao
  * @Date 15:04 2018/6/12
  */
-public class ACK3Header extends ACK2Header {
+@Setter
+@Getter
+public class ACK3Header implements ACKHeader {
+
+    @Order(order = 1)
+    private String recordType;
+    @Order(order = 2)
+    private String dbsMessageId;
+    @Order(order = 3)
+    private String originalMessageId;
+    @Order(order = 4)
+    private String fileCreationDateTime;
+    @Order(order = 5)
+    private String organizationID;
+    @Order(order = 6)
+    private String senderName;
+    @Order(order = 7)
+    private String groupStatus;
+    @Order(order = 8)
+    private String additionalInformation;
+
+
 }
