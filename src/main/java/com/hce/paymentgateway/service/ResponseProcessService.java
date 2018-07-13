14f3495f417331877d142c3fe5caa141dafe244d
Qiang Ma <mq7253180@126.com>
2018-07-13 19:02:27 +0800

    xxx

--- src/main/java/com/hce/paymentgateway/service/ResponseProcessService.java
+++ src/main/java/com/hce/paymentgateway/service/ResponseProcessService.java
@@ -1,10 +1,8 @@
 package com.hce.paymentgateway.service;
 
 import java.io.File;
-import java.io.IOException;
-import java.text.ParseException;
 import java.util.List;
 
 public interface ResponseProcessService {
-	public void process(List<File> files) throws IOException, ParseException;
+	public void process(List<File> files);
 }
