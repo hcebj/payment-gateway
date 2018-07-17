c346e9a17a0c5ff36f7bd305c3d163ae6a12cf30
Qiang Ma <mq7253180@126.com>
2018-07-17 14:23:35 +0800

    xxx

---
+++ src/main/java/com/hce/paymentgateway/dao/PayBaseInfoDao.java
@@ -0,0 +1,18 @@
+package com.hce.paymentgateway.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.hce.paymentgateway.entity.PayBaseInfoEntity;
+
+@Repository
+public interface PayBaseInfoDao extends JpaRepository<PayBaseInfoEntity, Long> {
+	@Modifying
+	@Query("UPDATE PayBaseInfoEntity t SET t.fieldValue=:fieldValue WHERE t.id=:id and t.fileStatus=:fileStatus")
+	public  int updateById(@Param(value="id")Long id, @Param(value="fieldValue") String fieldValue, @Param(value="fileStatus") String fileStatus);
+	
+	public  PayBaseInfoEntity findByFieldName(String corp, String fieldName, String fileType);
+}
---
+++ src/main/java/com/hce/paymentgateway/entity/PayBaseInfoEntity.java
@@ -0,0 +1,37 @@
+package com.hce.paymentgateway.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "pay_base_info")
+@EntityListeners(AuditingEntityListener.class)
+public class PayBaseInfoEntity {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name="corp")
+	private String corp;
+	@Column(name="field_name")
+	private String fieldName;
+	@Column(name="field_type")
+	private String fieldType;
+	@Column(name="field_value")
+	private String fieldValue;
+	@Column(name="field_status")
+	private String fieldStatus;
+
+}
