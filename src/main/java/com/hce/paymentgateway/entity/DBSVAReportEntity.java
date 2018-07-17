92f9430148b14c31b4e7cfa44f273db7dedf2203
Qiang Ma <mq7253180@126.com>
2018-07-17 09:49:02 +0800

    xxx

--- src/main/java/com/hce/paymentgateway/entity/DBSVAReportEntity.java
+++ src/main/java/com/hce/paymentgateway/entity/DBSVAReportEntity.java
@@ -28,8 +28,8 @@ public class DBSVAReportEntity {
     private Long id;
 	@Column(name="response_file")
 	private String responseFile;//响应结果所在文件名
-	@Column(name="parent_id")
-	private String parentId;//PARENT ID, 海云汇香港、海云汇国际
+	@Column(name="customer_id")
+	private String customerId;//CUSTOMER ID, 海云汇香港、海云汇国际
 	@Column(name="type")
 	private int type;//1VA Report (End-Of-Day); 2VA Report (30-min interval)
 	@CreatedDate
